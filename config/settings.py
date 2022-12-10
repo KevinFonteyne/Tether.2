7e6f55368d32ceb684f15a1a4ea30cf0173f7f83
corymfonteyne <corymfonteyne@gmail.com>
2022-12-10 14:48:30 -0500

    static files update

--- config/settings.py
+++ config/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 # STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles').replace('\\', '/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
